ffe6c54fc5fe6847cdb5c356b7d296ada304fad8
MetalMatze <me@metalmatze.de>
2015-05-04 16:02:11 +0200

    Rename the ToDo model to Todo for better writability

--- EasyTodoLib/models.py
+++ EasyTodoLib/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class ToDo(models.Model):
+class Todo(models.Model):
     title = models.CharField(max_length=128)
     deadline = models.DateField()
-    doneInPercent = models.DecimalField(max_digits=20, decimal_places=0)
+    done = models.PositiveIntegerField()
