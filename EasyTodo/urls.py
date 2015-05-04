2c9b6645c0752c6e218764604151f5e8efd26a07
MetalMatze <me@metalmatze.de>
2015-05-04 15:44:40 +0200

    Write url import without python's import

--- EasyTodo/urls.py
+++ EasyTodo/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from EasyTodoLib import urls as EasyTodoLibUrls
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^todos/', include(EasyTodoLibUrls))
+    url(r'^todos/', include('EasyTodoLib.urls'))
 ]
