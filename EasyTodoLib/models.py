2c18d61b19515456d74c63449c6b2618d05787a5
fabian <fabian082yang@hotmail.com>
2015-05-04 13:40:57 +0200

    1 add new templates folder which contains the whole html file  2 add IndexView and CreateToDo classes to the EasyToDoLib.views  3add ToDo class to EasyToDoLib.models

--- EasyTodoLib/models.py
+++ EasyTodoLib/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=128)
+    deadline = models.DateField()
+    doneInPercent = models.DecimalField(max_digits=20, decimal_places=0)
--- EasyTodoLib/views.py
+++ EasyTodoLib/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.views.generic import ListView
+from EasyToDoLib.models import ToDo
 # Create your views here.
+
+class IndexView(ListView):
+	template_name = 'home.html'
+	model = ToDo
+	
+class CreateToDo(CreateView):
+	template_name = 'new.html'
+	model = ToDo
+	success_url = '/'
