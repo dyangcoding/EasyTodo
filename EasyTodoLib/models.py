963143f576f157175f016d49a727705a67ba1bfc
MetalMatze <me@metalmatze.de>
2015-05-07 18:43:03 +0200

    Add validation to Todo.done

--- EasyTodoLib/models.py
+++ EasyTodoLib/models.py
@@ -1,7 +1,11 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=128)
     deadline = models.DateField()
-    done = models.PositiveIntegerField(default=0)
+    done = models.PositiveIntegerField(default=0, validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+    ])
